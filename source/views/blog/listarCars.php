ffaf8c9da6fe459384bb6e289a071adcbb526527
Isaque Menezes <isaque-menezes@hotmail.com>
2022-05-30 12:56:06 -0300

    final MVC PHP

--- source/views/blog/listarCars.php
+++ source/views/blog/listarCars.php
@@ -1,2 +1,15 @@
 <?php
-    echo " views/listarcars.php";
+    // var_dump($this->dados);
+
+    foreach($this->dados['cars'] as $car) {
+        extract($car);
+
+        // var_dump($;car);
+
+        echo "ID: " .$Id. "<br/>";
+        echo "MARCA: " .$marca. "<br/>";
+        echo "MODELO: " .$modelo. "<br/>";
+        echo "ANO: " .$ano. "<br/>";
+        echo "<hr/>";
+
+    }
